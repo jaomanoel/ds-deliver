910bc02d59fd057f8c0fd3e476036b3471a0419a
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-02-21 08:40:48 -0300

    Ready to prod

--- backend/src/main/resources/db/migration/V1__created_table_product.sql
+++ backend/src/main/resources/db/migration/V1__created_table_product.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tb_product (
     id SERIAL PRIMARY KEY,
     name varchar(255),
-    price DOUBLE,
+    price DOUBLE PRECISION,
     image_Uri varchar(255),
     description varchar(255)
 );
--- backend/src/main/resources/db/migration/V2__created_table_order.sql
+++ backend/src/main/resources/db/migration/V2__created_table_order.sql
@@ -1,8 +1,8 @@
 CREATE TABLE tb_order (
     id SERIAL PRIMARY KEY,
     address varchar(255),
-    latitude DOUBLE,
-    longitude DOUBLE,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
     moment TIMESTAMP,
     status SMALLINT
 );
